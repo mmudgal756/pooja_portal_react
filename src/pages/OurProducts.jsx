4a180a77f0dbcdbb49b3b041465e080089a8350f
mmudgal756 <mmudgal756@gmail.com>
2025-09-10 08:58:49 +0000

    xx

--- src/pages/OurProducts.jsx
+++ src/pages/OurProducts.jsx
@@ -52,7 +52,7 @@ function OurProducts() {
       {!loading && !error && (
         <Grid container spacing={4}>
           {products.map((product) => (
-            <Grid item key={product._id} xs={12} sm={6} md={4}>
+            <Grid item key={product._id} xs={12} sm={6} md={4} lg={4}>
               <Card sx={{ 
                 height: '100%', 
                 display: 'flex', 
